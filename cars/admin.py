06f0d6337c483504c494aec8a7085de6787f1b46
paulinatomusiak <p.tomusiak2@gmail.com>
2025-01-12 23:31:30 +0100

    update templates, adding static files, handling the photo field in the car model

--- cars/admin.py
+++ cars/admin.py
@@ -4,9 +4,22 @@ from cars.models import Car, Reservation
 
 # Register your models here.
 
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ["id", "is_available", "brand", "model", "year"]
-    fields = ["brand", "model", "year", "daily_rate", "doors", "car_type", "transmission", "seats", "fuel_type", "trunk_size"]
+    fields = [
+        "brand",
+        "model",
+        "year",
+        "daily_rate",
+        "doors",
+        "car_type",
+        "transmission",
+        "seats",
+        "fuel_type",
+        "trunk_size",
+        "image",
+    ]
     readonly_fields = ["is_available"]
 
 
--- cars/models.py
+++ cars/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from management.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-
+import os
+import random
 
 
 class Car(models.Model):
@@ -31,8 +32,9 @@ class Car(models.Model):
 
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
-    year = models.PositiveIntegerField(validators=[MinValueValidator(1999), MaxValueValidator(2025)]
-)
+    year = models.PositiveIntegerField(
+        validators=[MinValueValidator(1999), MaxValueValidator(2025)]
+    )
     daily_rate = models.DecimalField(max_digits=10, decimal_places=2)
     doors = models.PositiveIntegerField(choices=DOORS_CHOICES)
     car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
@@ -40,14 +42,36 @@ class Car(models.Model):
     seats = models.PositiveIntegerField(choices=SEATS_CHOICES)
     fuel_type = models.CharField(max_length=10, choices=FUEL_TYPE_CHOICES)
     trunk_size = models.PositiveIntegerField(help_text="Pojemność bagażnika w litrach")
+    image = models.CharField(max_length=255, blank=True, null=True)
     is_available = models.BooleanField(default=True)
 
     @property
-    def name(self):
+    def name(self) -> str:
         return f"{self.brand} {self.model}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.brand} {self.model} ({self.year}) Samochód {self.name} id: {self.id}"
+
+    @staticmethod
+    def assign_random_photo() -> str | None:
+        photos_dir = os.path.join("cars", "static", "car_photos")
+        valid_extensions = {".jpeg", ".jpg", ".png"}
+
+        photos = [
+            file
+            for file in os.listdir(photos_dir)
+            if os.path.isfile(os.path.join(photos_dir, file))
+            and os.path.splitext(file)[1].lower() in valid_extensions
+        ]
+
+        if photos:
+            return f"car_photos/{random.choice(photos)}"
+        return None
+
+    def save(self, *args: tuple, **kwargs: dict) -> None:
+        if not self.image:
+            self.image = self.assign_random_photo()
+        super().save(*args, **kwargs)
 
 
 class Reservation(models.Model):
@@ -56,5 +80,5 @@ class Reservation(models.Model):
     start_date = models.DateField(verbose_name="Start of reservation")
     end_date = models.DateField(verbose_name="End of reservation")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Reservation {self.id} car {self.car}"
