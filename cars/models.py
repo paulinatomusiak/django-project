7ab4a1d92bb9ec10c54b03b97909c43d45732778
paulinatomusiak <p.tomusiak2@gmail.com>
2025-01-12 04:42:10 +0100

    change list and detail views templates

--- cars/models.py
+++ cars/models.py
@@ -1,44 +1,45 @@
 from django.db import models
 from management.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
 
 class Car(models.Model):
     TRANSMISSION_CHOICES = [
-        ('manual', 'Manual'),
-        ('automatic', 'Automatic'),
+        ("manual", "Manualna"),
+        ("automatic", "Automatyczna"),
     ]
 
     FUEL_TYPE_CHOICES = [
-        ('petrol', 'Petrol'),
-        ('diesel', 'Diesel'),
-        ('electric', 'Electric'),
-        ('hybrid', 'Hybrid'),
+        ("petrol", "Benzyna"),
+        ("diesel", "Diesel"),
+        ("electric", "Elektryczny"),
+        ("hybrid", "Hybrydowy"),
     ]
 
     CAR_TYPE_CHOICES = [
-        ('sedan', 'Sedan'),
-        ('hatchback', 'Hatchback'),
-        ('suv', 'SUV'),
-        ('coupe', 'Coupe'),
-        ('van', 'Van'),
+        ("sedan", "Sedan"),
+        ("hatchback", "Hatchback"),
+        ("suv", "SUV"),
+        ("coupe", "Coupe"),
+        ("convertible", "Kabriolet"),
+        ("wagon", "Kombi"),
     ]
 
-    DOORS_CHOICES = [
-        (2, '2 doors'),
-        (3, '3 doors'),
-        (4, '4 doors'),
-        (5, '5 doors'),
-    ]
+    DOORS_CHOICES = [(2, 2), (3, 3), (4, 4), (5, 5)]
+    SEATS_CHOICES = [(2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9)]
 
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
-    year = models.PositiveIntegerField()
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1999), MaxValueValidator(2025)]
+)
     daily_rate = models.DecimalField(max_digits=10, decimal_places=2)
     doors = models.PositiveIntegerField(choices=DOORS_CHOICES)
     car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
     transmission = models.CharField(max_length=10, choices=TRANSMISSION_CHOICES)
-    seats = models.PositiveIntegerField()
+    seats = models.PositiveIntegerField(choices=SEATS_CHOICES)
     fuel_type = models.CharField(max_length=10, choices=FUEL_TYPE_CHOICES)
-    trunk_size = models.PositiveIntegerField(help_text="Trunk capacity in liters")
+    trunk_size = models.PositiveIntegerField(help_text="Pojemność bagażnika w litrach")
     is_available = models.BooleanField(default=True)
 
     @property
@@ -53,7 +54,7 @@ class Reservation(models.Model):
     car = models.ForeignKey(Car, on_delete=models.PROTECT, related_name="reservations")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField(verbose_name="Start of reservation")
-    end_date = models.DateField(verbose_name="End of reservationi")
+    end_date = models.DateField(verbose_name="End of reservation")
 
     def __str__(self):
         return f"Reservation {self.id} car {self.car}"
