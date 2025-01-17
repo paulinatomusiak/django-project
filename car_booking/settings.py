791e0991e44b904168f0fe6531f627d2fed03044
paulinatomusiak <p.tomusiak2@gmail.com>
2025-01-17 01:33:29 +0100

    change allowed domains

--- car_booking/settings.py
+++ car_booking/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@&d+6%4rgj1uaq)($4k-d_$8e(c8!d#(@oyy)$!eul49xd-d17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['18.225.92.221', 'ip-172-31-12-218.us-east-2.compute.internal']
+ALLOWED_HOSTS = []
 
 
 # Application definition
